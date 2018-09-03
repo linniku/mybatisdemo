917e32320d10ff8ecb41a1f5702b6b6da5d69447
yuhaitao <linniku123@163.com>
2018-09-03 23:02:46 +0800

    配置spring

--- src/main/java/com/yht/DemoTest.java
+++ src/main/java/com/yht/DemoTest.java
@@ -1,31 +1,27 @@
 package com.yht;
 
 import com.yht.dao.UserDao;
-import com.yht.domain.User;
-import com.yht.mapper.UserMapper;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.yht.dao.UserDao2;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.io.IOException;
-import java.io.InputStream;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * 2018/9/2 17:56
  **/
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 public class DemoTest {
 
     @Test
     public void testSpring() {
-        ApplicationContext context=new ClassPathXmlApplicationContext("classpath:sping-mybatis.xml");
+        ApplicationContext context=new ClassPathXmlApplicationContext("classpath:spring-mybatis.xml");
         UserDao userDao= (UserDao) context.getBean("userDao");
         System.out.println(userDao.selectById(1));
+        UserDao2 userDao2= (UserDao2) context.getBean("userDao2");
+        System.out.println(userDao2.selectById(1));
     }
 }
--- src/main/java/com/yht/dao/UserDao2.java
+++ src/main/java/com/yht/dao/UserDao2.java
@@ -1,22 +1,17 @@
 package com.yht.dao;
 
 import com.yht.domain.User;
-import com.yht.mapper.UserMapper;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by kingdee on 2018/9/3.
  */
-public class UserDao {
+public class UserDao2 extends SqlSessionDaoSupport{
     private static String NAMESPACE = "com.yht.mapper.UserMapper";
-//    @Autowired
-//    UserMapper userMapper;
-    @Autowired
-    SqlSessionTemplate sessionTemplate;
     public User selectById(int id) {
-//        User user = userMapper.selectById(id);
-        User user =sessionTemplate.selectOne(NAMESPACE+".selectById",id);
+        User user =getSqlSession().selectOne(NAMESPACE+".selectById",id);
         return user;
     }
 }
